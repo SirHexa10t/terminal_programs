84c7ece98a22006747ec71062baf3d0514d6a360
SirHexalot <sirhexal@gmail.com>
2025-07-20 11:56:45 +0300

    Improved table formatter's compilation

--- table_formatter/COMPILE.sh
+++ table_formatter/COMPILE.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-cargo build
+# opt-level: 3: maximum speed   s: optimize for size   z: even smaller
+
+time RUSTFLAGS="-C opt-level=3 -C target-cpu=native" cargo build --release && \
+  echo "SUCCESSFULLY BUILT. OUTPUT:"
+  ls -lh --color=always --time-style="+%F_%T" target/release/
--- table_formatter/TEST.sh
+++ table_formatter/TEST.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-cargo test --features cli_tests
+cargo test --features cli_tests && \
+  {
+    echo "FINISHED DEBUG TESTING SUCCESSFULLY, RUNNING RELEASE TESTS:"
+    echo "==========================================================="
+    cargo test --release --features cli_tests
+  }
+
